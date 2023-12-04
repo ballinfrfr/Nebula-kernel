8522f2dd3b9d32a6d1bec9ce710013f062461c30
aiden k <128117259+ballinfrfr@users.noreply.github.com>
2023-12-03 21:41:19 -0800

    Add files via upload

---
+++ drivers/input/keys-e.h
@@ -0,0 +1,87 @@
+#define A 0x01
+#define B 0x02
+#define C 0x04
+#define D 0x08
+#define E 0x09
+#define F 0x10
+#define G 0x11
+#define H 0x12
+#define I 0x13
+#define J 0x14
+#define K 0x15
+#define L 0x16
+#define M 0x17
+#define N 0x18
+#define O 0x19
+#define P 0x20
+#define Q 0x21
+#define R 0x22
+#define S 0x23
+#define T 0x24
+#define U 0x25
+#define V 0x26
+#define W 0x27
+#define X 0x28
+#define Y 0x29
+#define Z 0x30
+
+#define a 0x31
+#define b 0x32
+#define c 0x33
+#define d 0x34
+#define e 0x35
+#define f 0x36
+#define g 0x37
+#define h 0x38
+#define i 0x39
+#define j 0x3A
+#define k 0x3B
+#define l 0x3C
+#define m 0x3D
+#define n 0x3E
+#define o 0x3F
+#define p 0x40
+#define q 0x41
+#define r 0x42
+#define s 0x43
+#define t 0x44
+#define u 0x45
+#define v 0x46
+#define w 0x47
+#define x 0x48
+#define y 0x49
+#define z 0x4A
+
+#define EXCLAMATION 0x4B     // !
+#define AT 0x4C             // @
+#define HASH 0x4D           // #
+#define DOLLAR 0x4E         // $
+#define PERCENT 0x4F        // %
+#define CARET 0x50           // ^
+#define AMPERSAND 0x51       // &
+#define ASTERISK 0x52        // *
+#define OPEN_PARENTHESIS 0x53 // (
+#define CLOSE_PARENTHESIS 0x54 // )
+#define UNDERSCORE 0x55      // _
+#define PLUS 0x56            // +
+#define MINUS 0x57           // -
+#define EQUAL 0x58           // =
+#define OPEN_BRACKET 0x59    // {
+#define CLOSE_BRACKET 0x5A   // }
+#define OPEN_SQUARE_BRACKET 0x5B // [
+#define CLOSE_SQUARE_BRACKET 0x5C // ]
+#define COLON 0x5D           // :
+#define SEMICOLON 0x5E       // ;
+#define QUOTE 0x5F           // '
+#define DOUBLE_QUOTE 0x60    // "
+#define LESS_THAN 0x61       // <
+#define GREATER_THAN 0x62    // >
+#define COMMA 0x63           // ,
+#define PERIOD 0x64          // .
+#define SLASH 0x65           // /
+#define QUESTION_MARK 0x66   // ?
+#define BACKSLASH 0x67       // \
+#define PIPE 0x68            // |
+#define TILDE 0x69           // ~
+
+
---
+++ test.c
@@ -0,0 +1,13 @@
+#include <drivers/input/keys-e.h>
+#include <lib/std.h>
+
+
+#define hello "Hello, World!\n"
+
+int main() {
+    
+    printf(hello);
+   
+    
+    return 0;
+}
